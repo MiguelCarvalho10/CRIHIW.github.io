545def60921420ca40871ecdc906a8f98cac01fc
MiguelHSC <miguel.carvalho10@etec.sp.gov.br>
2024-11-13 23:32:20 -0300

    Update supabase.php

--- supabase.php
+++ supabase.php
@@ -1,5 +1,6 @@
 <?php
 // URL e chave da API do Supabase
+import { createClient } from '@supabase/supabase-js'
 $supabase_url = "https://sidvugqnlopeaikwlzov.supabase.co";  // Substitua pela sua URL do Supabase
 $supabase_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpZHZ1Z3FubG9wZWFpa3dsem92Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMTA4MTc4MiwiZXhwIjoyMDQ2NjU3NzgyfQ.JhcP9VwyRQb28pcFuWSa3fUpjzlcgPYKisDB7Cx3HDg";  // Substitua pela sua API Key
 
