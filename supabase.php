a3aca9d8a8efe5f4b2c13ed8432cb2c0071841d1
MiguelHSC <miguel.carvalho10@etec.sp.gov.br>
2024-11-14 00:35:56 -0300

    Update supabase.php

--- supabase.php
+++ supabase.php
@@ -1,7 +1,7 @@
 <?php
 // URL e chave da API do Supabase
 $supabase_url = "https://sidvugqnlopeaikwlzov.supabase.co";  // Substitua pela sua URL do Supabase
-$supabase_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpZHZ1Z3FubG9wZWFpa3dsem92Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMTA4MTc4MiwiZXhwIjoyMDQ2NjU3NzgyfQ.JhcP9VwyRQb28pcFuWSa3fUpjzlcgPYKisDB7Cx3HDg";  // Substitua pela sua API Key
+$supabase_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpZHZ1Z3FubG9wZWFpa3dsem92Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEwODE3ODIsImV4cCI6MjA0NjY1Nzc4Mn0.1jHUxTElCeHIAqCGbHvcc2RNruGPzY2DhXJ2c7Gab1Y";  // Substitua pela sua API Key
 
 // Configurações do cabeçalho para a API
 $options = [
