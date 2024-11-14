0a9fed5c798cbb89cf1d0167ff56ed6304dab660
MiguelHSC <miguel.carvalho10@etec.sp.gov.br>
2024-11-13 23:47:30 -0300

    Update conectar.php

--- conectar.php
+++ conectar.php
@@ -1,15 +1,15 @@
 <?php 
-$login = $_POST['login'];
-$senha = $_POST['senha'];
-$host = "sql305.infinityfree.com";                                      
-$user = "if0_37028164";                           
-$pass = "9FOeOgPXew";                                     
-$banco = "if0_37028164_clientes";                           
-$conexao = mysqli_connect($host, $user, $pass, $banco);  
-if (!$conexao) {                                          
- echo "Connection Error". PHP_EOL;
- echo "Error Code: ". mysqli_connect_errno().PHP_EOL;
- echo "Error: Description".mysqli_connect_error().PHP_EOL;
- exit;
+$host = "https://sidvugqnlopeaikwlzov.supabase.co";
+$port = "3000";
+$dbname = "usuarios";
+$user = "MiguelCarvalho10";
+
+try {
+    $conexao = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexão bem-sucedida com o Supabase!";
+} catch (PDOException $e) {
+    echo "Erro de Conexão: " . $e->getMessage();
 }
 ?>
+
